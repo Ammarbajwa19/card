a247894f54f914fbbf9d2168b6a782cb5df4b4e3
Ammarbajwa19 <ammarbajwah@gmail.com>
2022-12-13 21:48:40 +0500

    card

--- lib/data.dart
+++ lib/data.dart
@@ -2,43 +2,43 @@ import 'models/post.dart';
 
 final post = [
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile.jpg',
       postPicture: 'assets/post.jpg',
       timeAgo: '5 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile2.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '10 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile3.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '21 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile4.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '27 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile5.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '24 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile5.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '23 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile5.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '35 mint ago'),
   PostModel(
-      author: 'Mar BaJwa',
+      author: 'Mazhar BaJwa',
       profilePicture: 'assets/profile5.jpg',
       postPicture: 'assets/post.jpg',
-      timeAgo: '5 mint ago'),
+      timeAgo: '12 mint ago'),
 ];
